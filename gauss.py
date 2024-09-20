6b0322a7ebdc22fbe5f7b4eabca2b662b8e68462
shay <shay@utah.edu>
2024-09-20 12:55:55 -0600

    gaussian fit simple case success; continuous with q as well

--- gauss.py
+++ gauss.py
@@ -5,6 +5,8 @@ import matplotlib
 import matplotlib.widgets
 import warp as wp
 res = 100
+alpha = 1e-6
+epochs = 1000
 sigma0 = np.array([[5e-3, 0], [0, 1e-3]])
 sigma1 = sigma0
 x0 = np.array([0.4, 0.5])
@@ -24,11 +26,11 @@ def phi0_wp(x: wp.vec2) -> float:
 
 
 @wp.func 
-def phi_G_wp(x: wp.vec2, q: float, sigma: wp.array(dtype = wp.mat22), xbar: wp.array(dtype = wp.vec2)) -> float:    
+def phi_G_wp(x: wp.vec2, q: float, sigma: wp.array(dtype = wp.mat22), xbar: wp.array(dtype = wp.vec2), k: wp.array(dtype = float)) -> float:    
     phi = float(phi0_wp(x))
     for i in range(sigma.shape[0]):
         sgn = wp.select(i %  2== 0, 1.0, -1.0)
-        phi += q * sgn * gaussian_wp(sigma[i], x, xbar[i])
+        phi += q * sgn * gaussian_wp(sigma[i], x, xbar[i]) * k[i]
     return phi
 
 
@@ -51,21 +53,65 @@ y = np.linspace(0, 1, res)
 xs, ys = np.meshgrid(x, y)
 
 class Ellipsoid2D: 
-    def __init__(self, m = 4, q = None):
+    def __init__(self, m = 4, q = None, alpha = alpha):
         self.G = wp.zeros((m), dtype = wp.mat22, requires_grad= True)
         self.x = wp.zeros((m), dtype = wp.vec2, requires_grad = True)
+        self.k = wp.zeros((m), dtype = float, requires_grad= True)
         self.z = wp.zeros((res, res), dtype = float)
+        self.loss = wp.zeros((1), float, requires_grad = True)
+        self.m = m
+        self.q = q
         wp.launch(init, (m), inputs = [self.G, self.x])
+        self.alpha = alpha
 
     def phi_q(self, q):
-        wp.launch(phi_q_, (res, res), inputs = [self.G, self.x, q], outputs = [self.z])
-        return self.z.numpy()
+        wp.launch(phi_q_, (res, res), inputs = [self.G, self.x, q, self.z, self.k])
+        return self.z.numpy().T
+
+    def step(self, epoch, img):
+        tape = wp.Tape()
+        self.loss.zero_()
+        tape.zero()
+        with tape:
+            self.forward()
+        tape.backward(loss = self.loss)
+        self.update()
+        print(f"epoch {epoch}, loss = {self.loss.numpy()}")
+        # print(f"grad x = {self.x.grad.numpy()}")
+
+        # z = self.phi_q(1.0)
+        # plt.subplot(1, 1, 1)
+        # plt.contourf(x, y, z, levels = np.arange(-0.5, 0.5, 0.1), cmap = 'coolwarm')
+        # plt.plot(x, self.q.numpy(), 'b')
+
+
+        return img
+        
+    def forward(self):
+        wp.launch(loss, (res), inputs = [self.G, self.x, self.q, self.k, self.loss])
+
+    def update(self):
+       wp.launch(gradient_descend, (self.m), inputs = [self.G, self.x, self.k, self.G.grad, self.x.grad, self.k.grad, self.alpha]) 
 
 @wp.kernel
-def phi_q_(G: wp.array(dtype = wp.mat22), x: wp.array(dtype = wp.vec2), q: float, z: wp.array2d(dtype = float)):
+def gradient_descend(G: wp.array(dtype = wp.mat22), x: wp.array(dtype = wp.vec2), k: wp.array(dtype = float), G_grad: wp.array(dtype = wp.mat22), x_grad: wp.array(dtype = wp.vec2), k_grad: wp.array(dtype = float), alpha: float):
+    i = wp.tid()
+    G[i] -= alpha * 0.5 * (G_grad[i] + wp.transpose(G_grad[i]))
+    x[i] -= alpha * x_grad[i]
+    k[i] -= alpha * k_grad[i]
+
+@wp.kernel
+def loss(G: wp.array(dtype = wp.mat22), x: wp.array(dtype =wp.vec2), q_target: wp.array(dtype = float), k: wp.array(dtype = float), l: wp.array(dtype = float)):
+    i = wp.tid()
+    xi = wp.vec2(float(i) / float(res), q_target[i])
+    phii = phi_G_wp(xi, 1.0, G, x, k)
+    wp.atomic_add(l, 0, phii ** 2.0)
+
+@wp.kernel
+def phi_q_(G: wp.array(dtype = wp.mat22), x: wp.array(dtype = wp.vec2), q: float, z: wp.array2d(dtype = float), k: wp.array(dtype = float)):
     i, j = wp.tid()
     xi = wp.vec2(float(i) / float(res), float(j) / float(res))
-    z[i, j] = phi_G_wp(xi, q, G, x)
+    z[i, j] = phi_G_wp(xi, q, G, x, k)
 
 @wp.kernel
 def init(G: wp.array(dtype = wp.mat22), x: wp.array(dtype =wp.vec2)):
@@ -84,20 +130,44 @@ def phi_q(q):
 
 def express_ability_test():
     fig, ax = plt.subplots()
-    slider_ax = plt.axes([0.1, 0.1, 0.8, 0.03], facecolor="lightgray")  # Slider position
-    slider = matplotlib.widgets.Slider(slider_ax, 'q', -1.0, 1.0, valinit=0.0)
+    button = matplotlib.widgets.Button(plt.axes([0.1, 0.05, 0.1, 0.075]), 'Optimize')
 
     plt.subplot()
     z0 = phi_q(0.0)
     plt.contourf(x, y, z0, levels = np.arange(-0.5, 0.5, 0.1), cmap = 'coolwarm')
-    e2d = Ellipsoid2D()
-    
+    siny = np.sin(4 * np.pi * y) * 0.1 + 0.5
+    e2d = Ellipsoid2D(q = wp.from_numpy(siny, dtype = float, shape = (res)))
+
     def update(val):
         # z = phi_q(val)
         z = e2d.phi_q(val)
         plt.subplot(1, 1, 1)
         plt.contourf(x, y, z, levels = np.arange(-0.5, 0.5, 0.1), cmap = 'coolwarm')
+        plt.plot(x, siny, 'b')
 
+    # epoch = 0
+    # def optimize(event):
+    #     nonlocal epoch
+    #     e2d.step(epoch, 0)
+    #     epoch += 1
+    #     z = e2d.phi_q(1.0)
+    #     plt.subplot(1, 1, 1)
+    #     plt.contourf(x, y, z, levels = np.arange(-0.5, 0.5, 0.1), cmap = 'coolwarm')
+    #     plt.plot(x, siny, 'b')
+
+    # button.on_clicked(optimize)
+
+    for i in range(epochs):
+        e2d.step(i, 0)
+        if i % 50 == 0:
+            z = e2d.phi_q(1.0)
+            plt.subplot(1, 1, 1)
+            plt.contourf(x, y, z, levels = np.arange(-0.5, 0.5, 0.1), cmap = 'coolwarm')
+            plt.plot(x, siny, 'b')
+            plt.show()
+
+    slider_ax = plt.axes([0.1, 0.1, 0.8, 0.03], facecolor="lightgray")  # Slider position
+    slider = matplotlib.widgets.Slider(slider_ax, 'q', -1.0, 1.0, valinit=0.0)
     slider.on_changed(update)
     plt.show()
 
